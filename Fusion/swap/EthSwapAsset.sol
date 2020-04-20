2aab9b409780d8e51fe350c4a1c2f788a48c1caf
cross-chain <xiao.zongyuan@fusion.org>
2020-04-20 17:35:54 +0800

    add SwapAsset contract for cross chain bridge

---
+++ Fusion/swap/EthSwapAsset.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.5.0;
+
+import "../../ERC20/sol0.5/ERC20.sol";
+
+contract BtcSwapAsset is ERC20 {
+    event LogSwapin(bytes32 indexed txhash, address indexed account, uint amount);
+    event LogSwapout(address indexed account, uint amount, string bindaddr);
+
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "only owner");
+        _;
+    }
+
+    constructor() public ERC20("ETH Swap Asset", "fETH") {
+        owner = msg.sender;
+    }
+
+    function Swapin(bytes32 txhash, address account, uint256 amount) public onlyOwner returns (bool) {
+        _mint(account, amount);
+        emit LogSwapin(txhash, account, amount);
+        return true;
+    }
+
+    function Swapout(uint256 amount, string memory bindaddr) public returns (bool) {
+        _burn(_msgSender(), amount);
+        emit LogSwapout(_msgSender(), amount, bindaddr);
+        return true;
+    }
+}
